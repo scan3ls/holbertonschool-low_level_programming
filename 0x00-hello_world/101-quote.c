b0122c6d14ffce8d6b11534636c8600e74efb168
Logan Scanlon <scan3ls@gmail.com>
2020-02-05 22:39:11 +0000

    Add task to project

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.c>
+/**
+ * main - print a message
+ *
+ * Return: return a value of zero
+ */
+int main(void)
+{
+char str1[40] = "and that piece of art is useful\"\0";
+char str2[40] = " - Dora Korpar, 2015-10-19\n\0";
+fprintf(stderr, "%s%s", str1, str2);
+return (1);
+}

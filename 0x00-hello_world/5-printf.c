35e31ba98a2dddf1c827e5efb9928137abf08c09
Logan Scanlon <scan3ls@gmail.com>
2020-02-05 20:52:54 +0000

    Rewrite in Betty style

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-int main () {
-  printf("%s", "with proper grammar, but the outcome is a piece of art,\n\0");
-  return 0;
+/**
+ * main - Display a message with printf()
+ *
+ * Return: return a value of 0
+ */
+int main(void)
+{
+printf("%s", "with proper grammar, but the outcome is a piece of art,\n\0");
+return (0);
 }

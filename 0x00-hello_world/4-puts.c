02098121879b688ea4e3d3a55dee1669410b6f45
Logan Scanlon <scan3ls@gmail.com>
2020-02-05 20:02:29 +0000

    Correct output and remove new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main () {
-char str1[100] = "Programming is like building a multilingual puzzle\n";
+char str1[100] = "\"Programming is like building a multilingual puzzle";
 puts(str1);
 return 0;
 }

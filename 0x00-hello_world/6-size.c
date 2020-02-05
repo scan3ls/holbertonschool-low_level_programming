b04900a7a157e7f336b3ffd8e8bdf8fd0182b984
Logan Scanlon <scan3ls@gmail.com>
2020-02-05 21:15:09 +0000

    Change types passed to printf

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-printf("Size of a char: %lu\n", sizeof(char));
-printf("Size of an int: %lu\n", sizeof(int));
-printf("Size of a long int: %lu\n", sizeof(long int));
-printf("Size of a long long int: %lu\n", sizeof(long long int));
-printf("Size of a float: %lu\n", sizeof(float));
+printf("Size of a char: %u\n", sizeof(char));
+printf("Size of an int: %u\n", sizeof(int));
+printf("Size of a long int: %u\n", sizeof(long int));
+printf("Size of a long long int: %u\n", sizeof(long long int));
+printf("Size of a float: %u\n", sizeof(float));
 return (0);
 }

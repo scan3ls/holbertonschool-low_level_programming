9bce4502a05ffaf4283849dc100b6013658371fb
Logan Scanlon <scan3ls@gmail.com>
2020-02-06 21:01:59 +0000

    Add task to project

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -17,7 +17,7 @@ int main(void)
 	{
 		for (n2 = n1 + 1; n2 <= 9; n2++)
 		{
-			putchar((n1 / 10) + '0');
+			putchar((n1 % 10) + '0');
 			putchar((n2 % 10) + '0');
 			if (n1 == 8 && n2 == 9)
 				continue;

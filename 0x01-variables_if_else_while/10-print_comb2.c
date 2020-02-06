4f04e5f0e25104e289b34915ec9afec449f7ecce
Logan Scanlon <scan3ls@gmail.com>
2020-02-06 17:28:20 +0000

    Add task to project

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#define ZERO 48
+#define COMMA 44
+#define NINE 57
+#define NEWLINE 10
+#define SPACE 32
+/**
+ * main - print base 10 numbers seperated by commas from 00 - 99
+ *
+ * Return: Value of zero
+ */
+int main(void)
+{
+	int num1, num2;
+
+	for (num1 = ZERO; num1 <= NINE; num1++)
+	{
+		for (num2 = ZERO; num2 <= NINE; num2++)
+		{
+			putchar(num1);
+			putchar(num2);
+			putchar(SPACE);
+			if (num1 != NINE || num2 != NINE)
+			{
+				putchar(COMMA);
+			}
+		}
+	}
+	putchar(NEWLINE);
+	return (0);
+}

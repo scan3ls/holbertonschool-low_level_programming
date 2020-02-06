7fd67b4ff2c5f5fa0374945af5227e7dcc8ec5a1
Logan Scanlon <scan3ls@gmail.com>
2020-02-06 20:46:15 +0000

    Add task to project

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#define COMMA 44
+#define SPACE 32
+#define NEWLINE 10
+
+/**
+ * main - print all combinations of two digits
+ *
+ * Return: Value of zero
+ */
+
+int main(void)
+{
+	int n1;
+
+	for (n1 = 1; n1 <= 89; n1++)
+	{
+		putchar((n1 / 10) + '0');
+		putchar((n1 % 10) + '0');
+		if ( n1 != 89)
+		{
+		putchar(COMMA);
+		putchar(SPACE);
+		}
+	}
+
+	putchar(NEWLINE);
+
+	return (0);
+}

2905d0949d9987ff9329510b46db5eab9eb187e3
Logan Scanlon <scan3ls@gmail.com>
2020-02-11 16:42:04 +0000

    Add task to project

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+/**
+ * add - add two integer values and return the result
+ *@n1: first integer value.
+ *@n2: second integer value.
+ *
+ * Return: return added value of n1 and n2
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}

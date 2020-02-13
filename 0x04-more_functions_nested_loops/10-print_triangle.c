ec4c805168508ae6c472d7885a1df272dabb9dff
Logan Scanlon <1712@holbertonschool.com>
2020-02-13 20:27:17 +0000

    Add task to project

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+  * print_square - prints a triangle with # followed by a new line
+  *@size: height and length of the square
+  *
+  * Return: void
+  */
+
+void print_triangle(int size)
+{
+	int height, length;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (height = 0; height < size; height++)
+	{
+		for (length = 1; length <= size; length++)
+			_putchar(
+				(length >= (size - height))
+					? '#'
+					: ' '
+				);
+		_putchar('\n');
+	}
+}

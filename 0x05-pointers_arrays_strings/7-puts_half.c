7f4226b24a897a372f273f9b7d89f3898efb0404
Logan Scanlon <1712@holbertonschool.com>
2020-02-18 16:39:14 +0000

    Update

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,7 +13,7 @@ void puts_half(char *str)
 	int i;
 	int n = (len % 2 == 0) ? len / 2 : (len - 1) / 2;
 
-	for (i = 0; i <= n; i++)
+	for (i = 0; i < n; i++)
 	{
 		_putchar(str[(len - n) + i]);
 	}

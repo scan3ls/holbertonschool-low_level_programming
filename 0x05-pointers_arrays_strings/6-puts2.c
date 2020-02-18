442c9fc48af7a49cc3ae69ecad41a3c3c6b6f436
Logan Scanlon <1712@holbertonschool.com>
2020-02-18 15:06:53 +0000

    Delete trailing whitespace

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,12 +8,12 @@
   * Return: void
   */
 
-void puts2(char *str)	
+void puts2(char *str)
 {
 	int index = 0;
-	
+
 	while (*(str + index) != 0 && index < MAXLENGTH)
-	{	
+	{
 		if ((index % 2) == 0)
 			_putchar(*(str + index));
 		index++;

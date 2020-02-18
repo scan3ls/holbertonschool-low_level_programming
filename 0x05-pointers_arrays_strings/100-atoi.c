ce9a083cb514cda5a052b98b94a970e814c48deb
Logan Scanlon <scan3ls@gmail.com>
2020-02-18 23:26:08 +0000

    Condense logic and code

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,84 +9,38 @@
 
 int _atoi(char *s)
 {
-	int sum = getNum(s);
+	    int i = 0;
+	    int sign = 1;
+	    int sum = 0;
+	    int inNum = 0;
+		while (s[i] != '\0')
 
-	return (sum);
-}
-
-/**
-  * _pow - power a base number base by a power n
-  *@n: power used on base number
-  *@base: base number
-  *
-  * Return: integer of a power product
-  */
-
-int _pow(int base, int n)
-{
-	int prod = base;
-	int i;
-
-	for (i = 0; i < n; i++)
-		prod = prod * base;
-	return (prod);
-}
-
-/**
-  * getSign - get the sign of a string for conversion to int
-  *@s: string used
-  *@numStart: stop counting sign at numStart
-  *
-  * Return: int 1 if positive -1 if negative
-  */
-
-int getSign(char *s, int numStart)
-{
-	int i;
-	int sign = 0;
-
-	for (i = 0; i < numStart; i++)
 	{
 		if (s[i] == '-')
-			sign++;
+			sign *= -1;
+		if (isDigit(s[i]))
+		{
+			sum *= 10;
+			sum += s[i] - '0';
+			inNum = 1;
+		}
+		if (!(isDigit(s[i])) && inNum == 1)
+			break;
+		i++;
 	}
-	return (sign % 2 == 0 ? 1 : -1);
+	return (sign * sum);
 }
 
 /**
-  * getNum - get first complete number in a string
-  *@s: string checked
+  * isDigit - checks if char value is a number value in ascii
+  *@c: char value checked
   *
-  * Return: int value of number in string
+  * Return: 1 if char c is a digit. Otherwise 0
   */
 
-int getNum(char *s)
+int isDigit(char c)
 {
-	int i, multi;
-	int first = 0;
-	int last = 0;
-	int sum = 0;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			first = i;
-			while (s[i] >= '0' && s[i] <= '9')
-				i++;
-			last = i - 1;
-			break;
-		}
-	}
-
-	if (first == 0 && last == 0)
-		return (0);
-	i = first;
-
-	for (multi = (_pow(10, ((last - first)-1))); multi > 0; multi /= 10)
-	{
-		sum	+= ((s[i] - '0') * multi);
-		i++;
-	}
-	return (sum * getSign(s, first));
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
 }

4f9a6edc0f6e061a3ee8839ffddd7f3ea1bb85f7
Logan Scanlon <1712@holbertonschool.com>
2020-02-20 19:33:22 +0000

    Update

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -16,13 +16,13 @@ char *_strncpy(char *dest, char *src, int n)
 
 	while (src[i++] != '\0')
 		len++;
-	for (i = 0; i < (n - 1); i++)
+	for (i = 0; i < n; i++)
 	{
 		if (i < len)
 			dest[i] = src[i];
 		else
 			dest[i] = '\0';
 	}
-	dest[i] = '\0';
+
 	return (dest);
 }

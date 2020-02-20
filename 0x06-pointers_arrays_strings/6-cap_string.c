8dc21cd556e30e1e8485bdbceff2d101b4307b91
Logan Scanlon <1712@holbertonschool.com>
2020-02-20 21:35:00 +0000

    Update

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,29 +10,42 @@ char *cap_string(char *s)
 {
 	int i = 0;
 	int capNext = 1;
+
 	while (s[i] != 0)
 	{
 		if (!(inWord(s[i])))
-		      capNext = 1;
+			capNext = 1;
 		if ((s[i] >= 'A' && s[i] <= 'Z'))
 			capNext = 0;
 		if (_isLower(s[i]) && capNext)
-			{
-				s[i] -= 32;
-				capNext = 0;
-			}
+		{
+			s[i] -= 32;
+			capNext = 0;
+		}
 		i++;
 	}
 	return (s);
 
 }
-
+/**
+ * _isLower - checks for case
+ *@c: charcter used
+ *
+ * Return: true,1. False,2.
+ */
 int _isLower(char c)
 {
 	if (c >= 'a' && c <= 'z')
 		return (1);
 	return (0);
 }
+
+/**
+ * inWord - check if in word
+ *@c: character checked
+ *
+ * Return: True, 1. False, 2.
+ */
 
 int inWord(char c)
 {

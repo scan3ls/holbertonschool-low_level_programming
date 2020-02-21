4f1ea2cb7197cdce061625bf40c3a29fcff379f0
Logan Scanlon <1712@holbertonschool.com>
2020-02-21 00:21:19 +0000

    Update

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -8,6 +8,8 @@ char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
+int inWord(char c);
+int _isLower(char c);
 char *cap_string(char *);
 char *lett(char *);
 char *rot13(char *);

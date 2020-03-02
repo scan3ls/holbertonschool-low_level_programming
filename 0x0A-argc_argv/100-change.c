e3789a53142f84f621df7ff6c8044d2f14873321
Logan Scanlon <scan3ls@gmail.com>
2020-03-02 00:34:59 +0000

    Add task to project

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define QUARTER 25
+#define DIME 10
+#define NICKLE 5
+#define HOLB 2
+#define PENNY 1
+
+/**
+ * main - print the min. number of coins to make change for an amount of money
+ *@argc: argument count
+ *@argv: array of arguments
+ *
+ * Return: 1 if arguments passed is not 2
+ */
+
+int main(int argc, char *argv[])
+{
+	int count = 0;
+	int cents = atoi(argv[1]);
+
+	while (cents != 0)
+	{
+		if ((cents / QUARTER) > 0)
+		{
+			count++;
+			cents -= QUARTER;
+		}
+		else if ((cents / DIME) > 0)
+		{
+			count++;
+			cents -= DIME;
+		}
+		else if ((cents / NICKLE) > 0)
+		{
+			count++;
+			cents -= NICKLE;
+		}
+		else if ((cents / HOLB) > 0)
+		{
+			count++;
+			cents -= HOLB;
+		}
+		else
+		{
+			count++;
+			cents -= PENNY;
+		}
+	}
+
+	printf("%d\n", count);
+	return 0;
+}

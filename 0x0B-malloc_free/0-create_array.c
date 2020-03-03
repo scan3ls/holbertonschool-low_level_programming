d91f69cd294b514edd0f50fa0c303fd594c4b82f
Logan Scanlon <1712@holbertonschool.com>
2020-03-03 18:20:48 +0000

    Create project

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * create_array - creatse array of chars and fills with a single char value
+ *@size: size of the array
+ *@c: value to fill array with
+ *
+ * Return: pointer to the array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	/*allocates enough memory for the array*/
+	char *p = malloc(size * sizeof(int));
+	int i;
+
+	if (size <= 0)
+	{
+		return (0);
+	}
+	if (p)
+	{
+		for (i = 0; i < size; i++)
+		{
+			p[i] = c;
+		}
+	}
+}

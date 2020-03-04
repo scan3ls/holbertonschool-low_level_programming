85e350e86f00bd33b3a19417fbf1b2c2d550bd92
Logan Scanlon <1712@holbertonschool.com>
2020-03-04 18:09:46 +0000

    Add task

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - frees a 2d grid
+ *@grid: grid with allocated memory
+ *@height: number of pointers in **grid
+ *
+ * Return: void
+ */
+
+void free_grid(int **grid, int height)
+{
+	int column;
+
+	for (column = 0; column <= height; column++)
+	{
+		free(grid[column]);
+	}
+	free(grid);
+}

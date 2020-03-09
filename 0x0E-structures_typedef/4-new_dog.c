b571a4928bd562202870d030ef0a811ebd01be0c
Logan Scanlon <scan3ls@gmail.com>
2020-03-09 21:01:02 +0000

    Allocate memory for struct dog elements

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,42 @@
 #include <stdlib.h>
 #include "dog.h"
 
+/**
+ * _strlen - finds the length of a string
+ *@s: string to be used
+ *
+ * Return: length of the string
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*(s + i))
+		i++;
+	return (i);
+}
+
+/**
+ * _strcpy - copy a string to a destination
+ *@dest: destination of source
+ *@src: source string to be copied
+ *
+ * Return: *dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		*(dest + i) = src[i];
+		i++;
+	}
+	*(dest + i) = src[i];
+	return (dest);
+}
 
 /**
  * new_dog - return pointer to new dog
@@ -15,20 +51,35 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
 
-
 	if (name == NULL || owner == NULL)
 	{
-		exit(0);
 		return (NULL);
 	}
 
 	d = malloc(sizeof(struct dog));
 	if (d == NULL)
+	{
 		return (NULL);
+	}
 
-	d->name = name;
+	d->name = malloc(sizeof(char) * (_strlen(name) + 1));
+	if (d->name == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
+
+	d->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	if (d->owner == NULL)
+	{
+		free(d->name);
+		free(d);
+		return (NULL);
+	}
+
+	_strcpy(d->name, name);
 	d->age = age;
-	d->owner = owner;
+	_strcpy(d->owner, owner);
 
 	return (d);
 }

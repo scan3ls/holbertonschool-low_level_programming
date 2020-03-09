a158156a134e28a2c88b458c66182142c16709e8
Logan Scanlon <scan3ls@gmail.com>
2020-03-09 20:07:49 +0000

    Update

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,16 +1,25 @@
 #include <stdlib.h>
 #include "dog.h"
 
+/**
+ * new_dog - return pointer to new dog
+ *@name: dog name
+ *@age: dog age
+ *@owner: dog owner name
+ *
+ * Return: pointer to dog
+ */
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
 
 	d = malloc(sizeof(struct dog));
-	if (d == NULL)
+	if (d == NULL || name == NULL || owner == NULL)
 		return (NULL);
-	d->name = name;
+	d->name = n;
 	d->age = age;
-	d->owner = owner;
+	d->owner = n;
 
 	return (d);
 }

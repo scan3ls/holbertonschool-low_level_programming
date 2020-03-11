eec3f5a696cc8e458a2b59b9c0cb4ee4e615e195
Logan Scanlon <scan3ls@gmail.com>
2020-03-11 21:03:57 +0000

    Add task

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+#include <string.h>
+
+/**
+ * get_op_func - select the correct function to perform
+ *@s: operator passed
+ *
+ * Return: pointer to correct operator function
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+	while (i < 5)
+	{
+		if (!(strcmp(s, ops[i].op)))
+			return (ops[i].f);
+		i++;
+	}
+	printf("Error\n");
+	exit(99);
+}
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,41 @@
+#include "3-calc.h"
+
+/**
+ * main - preform simple calculations
+ *@argc: argument count
+ *@argv: argument array
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1, num2;
+	int (*operator)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error1\n");
+		exit(98);
+	}
+
+	if (get_op_func(argv[2]) == NULL)
+	{
+		printf("Error2\n");
+		exit(99);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	operator = get_op_func(argv[2]);
+
+	if ((argv[2] == "/" || argv[2] == "%") && num2 == 0)
+	{
+		printf("Error3\n");
+		exit(100);
+	}
+
+	printf("%d\n",(operator)(num1, num2));
+
+	return (0);
+}

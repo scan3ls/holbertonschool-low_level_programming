3053749e91bc9f092b7936397e95d13aa68b078b
Logan Scanlon <scan3ls@gmail.com>
2020-03-13 17:53:04 +0000

    Add task

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - prints numbers followed by a new line
+ *@separator: separator used to format
+ *@n: number of numbers to print
+ *
+ * Return: void
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	int argc;
+	va_list num_list;
+
+	va_start(num_list, n);
+
+	printf("%d", va_arg(num_list, int));
+	for (argc = 1; argc < n; argc++)
+	{
+		printf("%s%d", separator, va_arg(num_list, int));
+	}
+	va_end(num_list);
+	printf("\n");
+}

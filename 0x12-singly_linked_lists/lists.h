0637e6461bfa6a6dda40a0f020813b344cb6866b
Logan Scanlon <scan3ls@gmail.com>
2020-03-20 03:24:11 +0000

    Update

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -24,5 +24,6 @@ void print_element(unsigned int u, char *s);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 int _strlen(const char *str);
+list_t *add_node_end(list_t **head, const char *str);
 
 #endif

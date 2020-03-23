d9c1356facda10dafee879385665f88e7379706d
Logan Scanlon <scan3ls@gmail.com>
2020-03-23 18:35:45 +0000

    Add task

---
+++ 0x13-more_singly_linded_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a listint_t list
+ *@h: listint_t list
+ *
+ * Return: number of nodes.
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t num;
+
+	if (h == NULL)
+		return (0);
+	while (h != NULL)
+	{
+		num++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+
+	return (num);
+}

602e33d979c77b211eff9242569f287299e40366
Logan Scanlon <scan3ls@gmail.com>
2020-03-24 07:09:03 +0000

    Rename Directory

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a listint_t list
+ *@h: listint_t list
+ *
+ * Return: number of nodes.
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t num;
+
+	while (h != NULL)
+	{
+		num++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+
+	return (num);
+}
---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * listint_len - return the number of elements in a listint_t list
+ *@h: listint_t list
+ *
+ * Return: number of nodes.
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t num;
+
+	if (h == NULL)
+		return (0);
+	while (h != NULL)
+	{
+		num++;
+		h = h->next;
+	}
+
+	return (num);
+}

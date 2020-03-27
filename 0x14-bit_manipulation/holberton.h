260476b81955fb30592f7765b428208e4a5a5ad6
Logan Scanlon <scan3ls@gmail.com>
2020-03-27 00:12:54 +0000

    Update

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -1,10 +1,14 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+#include <unistd.h>
+
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 int getBinLen(const char *b);
 int _pow(int base, int power);
 void print_binary(unsigned long int n);
+int _putchar(char c);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);

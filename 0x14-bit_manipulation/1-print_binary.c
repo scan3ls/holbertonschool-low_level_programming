50777261da63fe349082ef6c1616c64a53c61f60
Logan Scanlon <scan3ls@gmail.com>
2020-03-27 00:07:44 +0000

    Add task

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * print_binary - converts & prints the binary number
+ *@n: decimal number
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned bit;
+	int startFlag = 0;
+
+	for (bit = 1 << 31; bit > 0; bit = bit >> 1)
+	{
+		if (n & bit)
+		{
+			_putchar('1');
+			startFlag = 1;
+		}
+		else
+		{
+			if (startFlag == 1)
+				_putchar('0');
+		}
+	}
+	if (startFlag == 0)
+		_putchar('0');
+}

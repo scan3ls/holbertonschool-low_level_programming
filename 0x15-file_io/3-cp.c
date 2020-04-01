10dc8ca4ec3b80f29e8868268c55c51d019b89f9
Logan Scanlon <scan3ls@gmail.com>
2020-04-01 02:21:18 +0000

    Rewrite to handle any file size

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,7 +3,7 @@
 #include "holberton.h"
 
 #define KB 1024
-#define MAX_SIZE (50 * KB)
+
 /**
  * main - copies contents from one file to another
  *@argc: number of arguments
@@ -14,8 +14,8 @@
 
 int main(int argc, char *argv[])
 {
-	int f_to, f_from;
-	char buf[MAX_SIZE];
+	int f_to, f_from, bufcount, check;
+	char buf[KB];
 	mode_t permissions = 00664;
 
 	if (argc != 3)
@@ -24,106 +24,48 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 
-	/* read source file */
-	f_from = read_file(argv[1], MAX_SIZE, buf);
+	f_from = open(argv[1], O_RDONLY);
 	if (f_from == -1)
 	{
-		dprintf(STDERR_FILENO,
-			"Error: Can't read from file %s\n",
-			argv[1]
-			);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	/* write copy file */
-	f_to = create_file2(argv[2], buf, permissions);
+
+	f_to = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, permissions);
 	if (f_to == -1)
 	{
-		dprintf(STDERR_FILENO,
-		       "Error: Can't write to %s\n",
-		       argv[2]
-			);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
+
+	while ((bufcount = read(f_from, buf, KB)) > 0)
+	{
+		check = write(f_to, buf, bufcount);
+		if (check == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+
+	hClose(f_from);
+	hClose(f_to);
 
 	return (0);
 }
 
 /**
- * read_file - read file and save content to buffer
- *@filename: file to read
- *@letters: len of the file
- *@buf: buffer to save content to
- *
- * Return: 1 on success, -1 on failure
+ * hClose - close file with error msg
+ *@fd: open file to close
  */
-
-ssize_t read_file(const char *filename, size_t letters, char *buf)
+void hClose(int fd)
 {
-	int fd, rd, flc;
+	int check;
 
-	if (filename == NULL)
-		return (-1);
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (-1);
-
-	rd = read(fd, buf, letters);
-
-	flc = close(fd);
-	if (flc == -1)
+	check = close(fd);
+	if (check == -1)
 	{
-		dprintf(STDERR_FILENO,
-		       "Error: Can't close fd %d\n",
-		       fd
-			);
+		dprintf(STDERR_FILENO, "Error: Can't close %d\n", fd);
 		exit(100);
 	}
-	return (rd);
-}
-
-/**
- * create_file2 - create a new file
- *@filename: name of new file
- *@text_content: content of new file
- *@permissions: permissions of new file
- *
- * Return: 1 on success, -1 on failure
- */
-
-int create_file2(const char *filename, char *text_content, mode_t permissions)
-{
-	int wd, fd, flc;
-	int len = 0;
-
-	if (filename == NULL)
-		return (-1);
-
-	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, permissions);
-
-	if (fd == -1)
-		return (-1);
-
-	if (text_content == NULL)
-	{
-		text_content = "";
-	}
-
-	while (text_content[len] != '\0')
-		len++;
-
-	wd = write(fd, text_content, len);
-
-	flc = close(fd);
-	if (flc == -1)
-	{
-		dprintf(STDERR_FILENO,
-		       "Error: Can't close fd %d\n",
-		       fd
-			);
-		exit(100);
-	}
-	if (wd == -1)
-		return (-1);
-
-	return (1);
 }
--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -9,7 +9,6 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-ssize_t read_file(const char *filename, size_t letters, char *buf);
-int create_file2(const char *filename, char *text_content, mode_t permissions);
+void hClose(int fd);
 
 #endif

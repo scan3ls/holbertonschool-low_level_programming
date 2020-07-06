d37ce72561629e57c0a54c55e34a67d202acb001
Logan Scanlon <scan3ls@gmail.com>
2020-07-06 20:07:50 +0000

    Check all indecies

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,35 +4,54 @@
 
 def island_perimeter(grid):
     "find island perimeters"
-    floor = len(grid) - 1
+    floor = len(grid)
     index = 0
     prim = 0
 
-    for layer in range(1, floor):
-        topLayer = grid[layer - 1]
+    for layer in range(0, floor):
+        try:
+            topLayer = grid[layer - 1]
+        except:
+            topLayer = None
+        try:
+            bottomLayer = grid[layer + 1]
+        except:
+            bottomLayer = None
+
         middleLayer = grid[layer]
-        bottomLayer = grid[layer + 1]
+        length = len(middleLayer) - 1
 
         for index, value in enumerate(middleLayer):
 
             if value == 0:
                 continue
 
-            top = topLayer[index]
-            bottom = bottomLayer[index]
-            left = middleLayer[index - 1]
-            right = middleLayer[index + 1]
+            top = None
+            if topLayer is not None:
+                top = topLayer[index]
 
-            if top == 0:
+            bottom = None
+            if bottomLayer is not None:
+                bottom = bottomLayer[index]
+
+            left = None
+            if index != 0:
+                left = middleLayer[index - 1]
+
+            right = None
+            if index != length:
+                right = middleLayer[index + 1]
+
+            if top == 0 or top is None:
                 prim += 1
 
-            if bottom == 0:
+            if bottom == 0 or bottom is None:
                 prim += 1
 
-            if left == 0:
+            if left == 0 or left is None:
                 prim += 1
 
-            if right == 0:
+            if right == 0 or right is None:
                 prim += 1
 
     return prim

fe33375d18f17bdcdf640c1c144b010fd974c701
scan3ls <scan3ls@gmail.com>
2020-08-25 20:56:41 +0000

    Add task

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -7,4 +7,8 @@
 /* 0-linear.c */
 int linear_search(int *array, size_t size, int value);
 
+/* 1-binary.c */
+int binary_search(int *array, size_t size, int value);
+void print_array(int *array, size_t left, size_t right);
+
 #endif /* SEARCH_ALGO */
